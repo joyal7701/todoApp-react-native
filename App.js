9ed5c0ee9bf4694d2c9e1fe1684da2d116316d28
Joyal Shah <joyalshah07@gmail.com>
2020-11-29 15:13:36 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from "react";
+import { Text, View, TextInput, Button } from "react-native";
+class App extends React.Component {
+  state = {
+    todo: [],
+    text: "",
+  };
+  addTodo = () => {
+    var newTodo = this.state.text;
+    var arr = this.state.todo;
+    arr.push(newTodo);
+    this.setState({ todo: arr, text: "" });
+  };
+  deleteTodo = (t) => {
+    var arr = this.state.todo;
+    var pos = arr.indexOf(t);
+    arr.splice(pos, 1);
+    this.setState({ todo: arr });
+  };
+  renderTodos = () => {
+    return this.state.todo.map((t) => {
+      return (
+        <Text
+          key={t}
+          onPress={() => {
+            this.deleteTodo(t);
+          }}
+        >
+          {t}
+        </Text>
+      );
+    });
+  };
+  render() {
+    return (
+      <View style={styles.viewStyle}>
+        <Text>Todo App!</Text>
+        <TextInput
+          style={styles.inputStyle}
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+        />
+        <Button title="Click me" onPress={this.addTodo} />
+        {this.renderTodos()}
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
+const styles = {
+  viewStyle: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+  inputStyle: {
+    height: 40,
+    borderColor: "green",
+    borderWidth: 1,
+    width: 300,
+  },
+};
+export default App;
